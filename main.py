987b36f4508f30861f761dd801b875f866430930
jcjmenez <jcjmenez.02@gmail.com>
2024-04-21 19:42:22 +0200

    Updated structure

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from key_handler import KeyHandler
 from lane_detector import LaneDetector
 
 # Load model
-model_handler = ModelHandler('yolov8n.pt')
+model_handler = ModelHandler('models/yolov8n.pt')
 
-video_path = 'dataset/videos/back3.mp4'
+video_path = 'datasets/videos/back3.mp4'
 video_handler = VideoHandler(video_path)
 lane_detector = LaneDetector()
 ret = True
